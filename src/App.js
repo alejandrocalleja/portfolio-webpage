d89b10a4718e7a78b462f038b0c90d02e0a45ce0
alejandrocalleja <alejandro.calleja@alumni.mondragon.edu>
2022-05-07 00:08:27 +0200

    logo added

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Work in progress...</p>
       </header>
     </div>
   );
 }
 
 export default App;
+
